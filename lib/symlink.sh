3d0647ab73151c455ca34d4b69e13a9b3bb50519
landonb <landonb@users.noreply.github.com>
2023-03-11 14:43:43 -0600

    Refactor: Split `symlink` to its own module, for reusability

---
+++ lib/symlink.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=bash
+# Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
+# Project: https://github.com/DepoXy/myrepos-mredit-command#🧜
+# License: MIT
+
+# Copyright (c) © 2020-2023 Landon Bouma. All Rights Reserved.
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# This is an overly-zealous DRY file.
+#
+# - The symlink fcn. is located here (and not in bin/infuse-configlns) so
+#   upstream depoxy-ambers can use without maintaining an identical copy.
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+symlink () {
+  local source="$1"
+
+  # Build the hierarchy.
+  local relative_dir
+  relative_dir="$(dirname "${source}" | sed 's#^/##')"
+
+  mkdir -p "${relative_dir}"
+
+  # Make the symlink.
+  local config_file
+  config_file="$(basename "${source}")"
+
+  local target="${relative_dir}/${config_file}"
+
+  /bin/ln -s "${source}" "${target}"
+
+  # `ln` happily makes symlink to non-existent target, which we
+  # will allow (which is a Good Thing, e.g., `rg` complains on
+  # broken links, which'll alert user to the problem). But we'll
+  # also check ourselves, to be proactive.
+  if [ -e "${source}" ]; then
+    let 'INFUSE_SYMLINKS_CNT += 1'
+  else
+    # This path unreachable in this script, but the `symlink`
+    # COPYD from DepoXy, so keeping similar (except this comment).
+    let 'INFUSE_SYMLINKS_NOK += 1'
+
+    >&2 echo "WARN: Phantom target symlinked: ${source}"
+    >&2 echo "- You'll see broken symlink at: $(pwd)/${target}"
+  fi
+}
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
+
+main () {
+  symlink "$@"
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# Only run when executed; no-op when sourced.
+if [ "$0" = "${BASH_SOURCE}" ]; then
+  main "$@"
+fi
+
